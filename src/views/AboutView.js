4161457ccc53935b5dcd0e8664a0d14ba37ea809
Attila Juhász <juhasz.attila@k-monitor.hu>
2022-03-29 09:58:20 +0200

    Update AboutView.js

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -16,6 +16,12 @@ const AboutView = (props) => {
             <p>Az <i>Eltették</i> oldalon térképen mutatjuk meg a vagyonkezelő alapítványoknak kijátszott ingatlanvagyont. Mivel az ingatlanok átadásáról a Parlament akkor döntött, amikor mindenki a járvánnyal volt elfoglalva, a magyar lakosság nagy része valószínűleg nem is tudja, hogy az, ami korábban a közös vagyonunk volt, ma már egy magánjogi szereplő által tulajdonolt épület, föld, kastély, múzeum. Azaz formálisan már nem a miénk. Az ingatlanokat a vagyonjuttatásról rendelkező jogszabályokból gyűjtöttük össze, helyrajzi szám alapján.</p>
           </section>
           <section>
+           <h2>A témáról megjelent írásaink</h>  
+            <li><a href="https://k.blog.hu/2021/07/17/kekva" target="new">Már nem a miénk: A NER-alapítványok vagyona</a> - <i>2021.07.17.</i></li>
+            <li><a href="https://k.blog.hu/2021/11/15/osszeferhetetlen_kekva-arisztokratak" target="new">Összeférhetetlen KEKVA-arisztokraták?</a> - <i>2021.11.15.</i></li>
+            <li><a href="https://k.blog.hu/2021/11/24/kekva_830" target="new">Mi történik az alapítványoknak kiszervezett közvagyonnal?</a> - <i>2021.11.24.</i></li>
+          </section>
+          <section>
            <h2>Az adatok számokban</h>  
             <p>Az ingatlanok és a hozzájuk tartozó adatok elsődleges forrása a KEKVÁkat létrehozó törvények, amelyek mellékletei tartalmazzák az átadásra kerülő állami tulajdonú ingatlanok felsorolását. A legtöbb esetben helyrajziszámot tartalmazó felsorolást a <a href="https://mepar.mvh.allamkincstar.gov.hu/" target="new">MePAR</a> segítségével alakítottuk földrajzi koordinátákká. Az adatbázist kiegészítettük továbbá az alapítványok kuratóriumi tagságával. Az ingatlanok közértetőbbé tétele érdekében - ahol arra lehetőség volt - hétköznapi elnevezéssel, képpel illetve kapcsolódó sajtócikkekkel láttuk el.</p>
             <p>A térképen megjelenített teljes adatbázis <b>23 közérdekű vagyonkezelő alapítvány, több mint 1400 inaglanját tartalmazza</b>. * A felsőoktatási épületeken túl ezek között akad számos beépítetlen terület, gazdasági épület, de sok esetben nemzetgazdaságilag és társadalmilag értékes kutatóközpont, műemlék, lakóház, üdülő is.</p>
