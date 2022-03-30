f3fbf000471416111f54c9b846b343d2c899a654
Attila Juhász <juhasz.attila@k-monitor.hu>
2022-03-30 08:43:43 +0200

    tartalom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,14 +24,6 @@ const AboutView = (props) => {
           <li><a href="https://adatbazis.k-monitor.hu/adatbazis/cimkek/varga-mihaly" target="new">Varga Mihály</a> - a Rudolf Kalman Óbudai Egyetemért Alapítvány kuratóriumi elnöke</li>
           <li><a href="https://adatbazis.k-monitor.hu/adatbazis/cimkek/orban-balazs" target="new">Orbán Balázs</a> - Mathias Corvinus Collegium Alapítvány kuratóriumi elnöke</li>
         </ul>
-        <h2>Már nem a miénk: eltették a közvagyont</h2>
-        <p>Sokak figyelmét elkerülhette, hogy <b>a járvány leple alatt ezer milliárd forint értékű állami vagyon került át ingyenesen a kormánypárthoz kötődő alapítványokhoz</b>. <Link to="/#info">*</Link> Egyetemek, földek, kastélyok, üdülők, kollégiumok, sportpályák, irodaházak váltak köztulajdonból egy magánjogi szereplő tulajdonává a Parlament kétharmados döntései nyomán.</p>
-        <p>Az alapítványok vezetőit a jelenlegi kormány nevezte ki, de a kurátorokat egy későbbi kormány már nem tudja leváltani. A puccsszerű vagyonkiszervezés valódi célja nem a kulturális-oktatási javak hatékonyabb kezelése, az intézmények függetlensége és stabilitása volt, hanem a jelenlegi politikai elit bebetonozása egy esetleges választási vereség esetére.</p>
-        <p>Interaktív térképen mutatjuk be az alapítványi tulajdonba került ingatlanvagyont.</p>
-        <h2>Már nem a miénk: eltették a közvagyont</h2>
-        <p>Sokak figyelmét elkerülhette, hogy <b>a járvány leple alatt ezer milliárd forint értékű állami vagyon került át ingyenesen a kormánypárthoz kötődő alapítványokhoz</b>. <Link to="/#info">*</Link> Egyetemek, földek, kastélyok, üdülők, kollégiumok, sportpályák, irodaházak váltak köztulajdonból egy magánjogi szereplő tulajdonává a Parlament kétharmados döntései nyomán.</p>
-        <p>Az alapítványok vezetőit a jelenlegi kormány nevezte ki, de a kurátorokat egy későbbi kormány már nem tudja leváltani. A puccsszerű vagyonkiszervezés valódi célja nem a kulturális-oktatási javak hatékonyabb kezelése, az intézmények függetlensége és stabilitása volt, hanem a jelenlegi politikai elit bebetonozása egy esetleges választási vereség esetére.</p>
-        <p>Interaktív térképen mutatjuk be az alapítványi tulajdonba került ingatlanvagyont.</p>
       </div>
     </Layout>
   );
