9d3c1dc5cfe784c1345fe396132a23f27d8f95a2
ejulcsi <ejulcsi@gmail.com>
2022-04-03 17:17:51 +0200

    tartalom fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ const AboutView = (props) => {
     <Layout history={props.history}>
       <div className={styles.about}>
           <section>
-           <h2>Nem kérték, nem indokolták, csak eltették.</h2>
+            <h2>Nem kérték, nem indokolták, csak eltették.</h2>
             <p>2022-ben a 2010 óta kétharmados többséggel kormányzó Fidesz-KDNP váratlan választási kihívással szembesült: az ellenzéki pártok összefogtak, hogy közösen induljanak ellene a választási rendszer sajátosságai miatt, így hosszú évek után először lett valós esélye annak, hogy Magyarország miniszterelnökét ne Orbán Viktornak hívják.</p>
             <p>A rezsim a fenyegetés hatására érdekes metamorfózison, egy perverz privatizációs hullámon ment keresztül. Miközben a 2010 utáni Orbán-kormányok visszaállamosításokkal, erőteljes központosításokkal az “erős állam” és a kormányzati hatalom maximalizálása mellett törtek pálcát, mint ami képes megoldani a társadalmi problémákat. 2020-ban azonban fordult a kocka, és a laikus szemlélő számára úgy tűnhetett: az állam és a kormány talán mégsem mindenható, nem biztos, hogy jó gazdája a közös vagyonunknak.</p>
             <p>A mindenkori kormánytól függetlenített intézmények, hatóságok sora jött létre, miközben magáncégek számára készítettek elő több évtizedes koncessziókat, tovább csökkentve a választott kabinetek mozgásterét. Az állami vagyon kiszervezésének központi eleme pedig a 2021. évi IX. törvény a közfeladatot ellátó közérdekű vagyonkezelő alapítványokról. A sorra létrehozott alapítványok (“KEKVÁ-k”) magánjogi szereplőként névleg a mindenkori kormánytól függetlenül látnak el kulturális-oktatási közfeladatokat, ehhez pedig pénzbeli vagyonjuttatást, részvénycsomagot és összesen sok ezer milliárd forint értékű ingatlanvagyont is kaptak ingyenesen a magyar államtól. A törvény ráadásul azt is előírja, hogy a mindenkori költségvetés tervezésekor előresorolt tényező kell legyen az alapítványok finanszírozása.</p>
@@ -16,22 +16,22 @@ const AboutView = (props) => {
             <p>Az <i>Eltették</i> oldalon térképen mutatjuk meg a vagyonkezelő alapítványoknak kijátszott ingatlanvagyont. Mivel az ingatlanok átadásáról a Parlament akkor döntött, amikor mindenki a járvánnyal volt elfoglalva, a magyar lakosság nagy része valószínűleg nem is tudja, hogy az, ami korábban a közös vagyonunk volt, ma már egy magánjogi szereplő által tulajdonolt épület, föld, kastély, múzeum. Azaz formálisan már nem a miénk. Az ingatlanokat a vagyonjuttatásról rendelkező jogszabályokból gyűjtöttük össze, helyrajzi szám alapján.</p>
           </section>
           <section>
-           <h2>A témáról megjelent írásaink</h2>
-           <ul>
+            <h2>A témáról megjelent írásaink</h2>
+            <ul>
             <li><a href="https://k.blog.hu/2021/07/17/kekva" target="new">Már nem a miénk: A NER-alapítványok vagyona</a> - <i>2021.07.17.</i></li>
             <li><a href="https://k.blog.hu/2021/11/15/osszeferhetetlen_kekva-arisztokratak" target="new">Összeférhetetlen KEKVA-arisztokraták?</a> - <i>2021.11.15.</i></li>
             <li><a href="https://k.blog.hu/2021/11/24/kekva_830" target="new">Mi történik az alapítványoknak kiszervezett közvagyonnal?</a> - <i>2021.11.24.</i></li>
             </ul>
           </section>
           <section>
-           <h2>Az adatokról</h2>  
+            <h2>Az adatokról</h2>  
             <p>Az ingatlanok és a hozzájuk tartozó adatok elsődleges forrása a KEKVÁkat létrehozó törvények, amelyek mellékletei tartalmazzák az átadásra kerülő állami tulajdonú ingatlanok felsorolását. A legtöbb esetben helyrajziszámot tartalmazó felsorolást a <a href="https://mepar.mvh.allamkincstar.gov.hu/" target="new">MePAR</a> segítségével alakítottuk földrajzi koordinátákká. Az adatbázist kiegészítettük továbbá az alapítványok kuratóriumi tagságával. Az ingatlanok közértetőbbé tétele érdekében - ahol arra lehetőség volt - hétköznapi elnevezéssel, képpel illetve kapcsolódó sajtócikkekkel láttuk el.</p>
             <p>A térképen megjelenített teljes adatbázis 23 közérdekű vagyonkezelő alapítvány, több mint 1400 inaglanját tartalmazza. * A felsőoktatási épületeken túl ezek között akad számos beépítetlen terület, gazdasági épület, de sok esetben nemzetgazdaságilag és társadalmilag értékes kutatóközpont, műemlék, lakóház, üdülő is.</p>
-            <p>Az ingatlanokat a vagyonjuttatásról rendelkező jogszabályokból gyűjtöttük össze, helyrajzi szám alapján. Emiatt a térképen <b>csak az alapítványi vagyonjuttatás egy része látható: mivel nem ingatlan, nem tüntettük fel az átadott részvénycsomagokat és készpénzt, illetve nem látszanak azok a főként határon túli ingatlanok, amelyeket cégeken (pl. MANEVI Zrt.) keresztül adtak oda alapítványoknak</b>. Szintén hiányoznak a térképről azok az ingatlanok, amelyeket a KEKVA tv. előtt juttattak bizonyos alapítványoknak (pl. <a href="https://24.hu/belfold/2020/04/01/schmidt-maria-alapitvany-budai-ingatlan-kap-ingyen-allamtol/" target="new">Közép- és Kelet-európai Történelem és Társadalom Kutatásáért Alapítvány).</p>
+            <p>Az ingatlanokat a vagyonjuttatásról rendelkező jogszabályokból gyűjtöttük össze, helyrajzi szám alapján. Emiatt a térképen <b>csak az alapítványi vagyonjuttatás egy része látható: mivel nem ingatlan, nem tüntettük fel az átadott részvénycsomagokat és készpénzt, illetve nem látszanak azok a főként határon túli ingatlanok, amelyeket cégeken (pl. MANEVI Zrt.) keresztül adtak oda alapítványoknak</b>. Szintén hiányoznak a térképről azok az ingatlanok, amelyeket a KEKVA tv. előtt juttattak bizonyos alapítványoknak (pl. <a href="https://24.hu/belfold/2020/04/01/schmidt-maria-alapitvany-budai-ingatlan-kap-ingyen-allamtol/" target="new">Közép- és Kelet-európai Történelem és Társadalom Kutatásáért Alapítvány)</a>.</p>
             <p>* <i>A mezőhegyesi ménesbirtokot működtető Jövő Nemzedék Földje Alapítvány 776, többségében mezőgazdasági művelés alatt álló ingatlanját mennyiségi korlátok miatt nem szerepeltetjük a térképen. Ezek mindegyike Mezőhegyes (Békés megye) területén fekszik.</i></p>
           </section>
           <section>
-           <h2>Köszönet</h2>  
+            <h2>Köszönet</h2>  
             <p>Az oldal fejlesztésében részt vevő <a href="https://k-monitor.hu/tevekenysegek/20200401-code-for-hungary" target="new">Code for Hungary</a> önkénteseknek, <a href="http://feren.cz/" target="new">Ferencz Miklósnak</a> (grafika).</p>
             <p><a href="http://vercel.com/" target="new">Hosted on Vercel.</a></p>
           </section>
